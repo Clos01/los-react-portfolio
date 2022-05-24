7a5da7adf5ce4e7b4d85f35a03120d21436bceae
Carlos Rivas <Cjrivas00@gmail.com>
2022-05-24 12:48:18 -0400

    adding work completetd skills

---
+++ src/components/Work.jsx
@@ -0,0 +1,121 @@
+import React from 'react'
+import budget from '../assets/budget.png'
+import Dash from '../assets/Dash.png'
+import friendless from '../assets/friendless.png'
+
+const Work = () => {
+  return (
+    <div name="Work " className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+            <div className='pb-8'>
+                  <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
+            <p className='py-6'>// Check out some of my recent work </p>
+            </div>
+          
+ {/* card conatiner     */}
+
+
+            <div
+                className='grid sm:grid-cols-2 md:grid-cols-3 gap-4 '>
+                <div style={{backgroundImage: `url(${budget})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* hover effects  */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <div>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+            React js app
+                            </span>
+                                <div className='pt-8 text-center'>
+                                    <a href="/">
+                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
+                                                        Demo
+                                                    </button>
+                                        </a>
+
+                                        <a href="/">
+                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
+                                                    Code
+                                                    </button>
+                                        </a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>           {/* card conatiner     */}
+
+
+             <div
+                className='grid sm:grid-cols-4 md:grid-cols-6gap-4 '>
+                <div style={{backgroundImage: `url(${ Dash })`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* hover effects  */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <div>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+            React js app
+                            </span>
+                                <div className='pt-8 text-center'>
+                                    <a href="/">
+                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
+                                                        Demo
+                                                    </button>
+                                        </a>
+
+                                        <a href="/">
+                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
+                                                    Code
+                                                    </button>
+                                        </a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+          
+
+             {/* card conatiner     */}
+
+
+             <div
+                className='grid sm:grid-cols-2 md:grid-cols-4 gap-1 '>
+                <div style={{backgroundImage: `url(${friendless})`}} 
+                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                    {/* hover effects  */}
+                    <div className='opacity-0 group-hover:opacity-100'>
+                        <div>
+                            <span className='text-2xl font-bold text-white tracking-wider'>
+            React js app
+                            </span>
+                                <div className='pt-8 text-center'>
+                                    <a href="/">
+                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
+                                                        Demo
+                                                    </button>
+                                        </a>
+
+                                        <a href="/">
+                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
+                                                    Code
+                                                    </button>
+                                        </a>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+
+  
+          
+
+
+
+
+    </div>
+  )
+}
+
+export default Work
