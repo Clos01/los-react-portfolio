4e5354d6535eb14031d0e734d5f81a9d0bea823b
Carlos Rivas <Cjrivas00@gmail.com>
2023-03-27 19:40:19 -0400

    didnt work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Home from "./components/Home.jsx";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Skills from "./components/Skills";
 import Work from "./components/Work";
