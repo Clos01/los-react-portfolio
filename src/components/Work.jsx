df70463621bada0a5d50277d3a467c6cbd7fb8fd
Carlos Rivas <Cjrivas00@gmail.com>
2023-03-27 19:04:39 -0400

    updated port

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,122 +1,108 @@
-import React from 'react'
-import budget from '../assets/budget.png'
-import Dash from '../assets/Dash.png'
-import friendless from '../assets/friendless.png'
+import React from "react";
+import rivas from "../assets/rivastp.png";
+import Dash from "../assets/Dash.png";
+import friendless from "../assets/friendless.png";
 
 const Work = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-            <div className='pb-8'>
-                  <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-            <p className='py-6'>// Check out some of my recent work </p>
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work </p>
+        </div>
+        {/* card conatiner     */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4  py-1 ">
+          {/* grid item  */}
+          <div
+            style={{ backgroundImage: `url(${rivas})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects  */}
+
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Budget Tracker
+              </span>
+              <div className="pt-8 text-center">
+                <a href="https://www.rivastpllc.com/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+
+                <a href="https://github.com/Clos01/rivastpllc">
+                  <button className="text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
             </div>
-          
- {/* card conatiner     */}
+          </div>
+        </div>{" "}
+        {/* card conatiner     */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4  py-2">
+          <div
+            style={{ backgroundImage: `url(${Dash})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div py-9"
+          >
+            {/* hover effects  */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div>
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Tech Blog
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://lostechblog.herokuapp.com/">
+                    <button className="text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
 
-            <div
-                className='grid sm:grid-cols-2 md:grid-cols-2 gap-4  py-1 '>
-                    {/* grid item  */}
-                <div style={{backgroundImage: `url(${budget})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-
-                    {/* hover effects  */}
-
-                <div className='opacity-0 group-hover:opacity-100'>
-                       
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-        Budget Tracker  
-                            </span>
-                            <div className='pt-8 text-center'>
-                                    <a href="https://clos01.github.io/Los-Budget-Tracker/">
-                                                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                                                        Demo
-                                                    </button>
-                                        </a>
-
-                                        <a href="https://github.com/Clos01/Los-Budget-Tracker">
-                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
-                                                    Code
-                                                    </button>
-                                        </a>
-
-                            </div>
-                     
-                    </div>
+                  <a href="https://github.com/Clos01/Tech-blog-los">
+                    <button className="text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
                 </div>
-            </div>           {/* card conatiner     */}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* card conatiner     */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-4 py-2 ">
+          <div
+            style={{ backgroundImage: `url(${friendless})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* hover effects  */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div>
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  friendless in Seattle
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="https://friendless-in-seattle.herokuapp.com/">
+                    <button className="text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
 
-
-             <div
-                className='grid sm:grid-cols-2 md:grid-cols-2 gap-4  py-2'>
-                <div style={{backgroundImage: `url(${ Dash })`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div py-9'>
-                    {/* hover effects  */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <div>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-                            Tech Blog
-                            </span>
-                                <div className='pt-8 text-center'>
-                                    <a href="https://lostechblog.herokuapp.com/">
-                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
-                                                        Demo
-                                                    </button>
-                                        </a>
-
-                                        <a href="https://github.com/Clos01/Tech-blog-los">
-                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
-                                                    Code
-                                                    </button>
-                                        </a>
-
-                            </div>
-                        </div>
-                    </div>
+                  <a href="https://github.com/Clos01/Friendless-in-seattle">
+                    <button className="text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg ">
+                      Code
+                    </button>
+                  </a>
                 </div>
+              </div>
             </div>
-          
-
-             {/* card conatiner     */}
-
-
-             <div
-                className='grid sm:grid-cols-2 md:grid-cols-2 gap-4 py-2 '>
-                <div style={{backgroundImage: `url(${friendless})`}} 
-                className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/* hover effects  */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <div>
-                            <span className='text-2xl font-bold text-white tracking-wider'>
-            friendless in Seattle 
-                            </span>
-                                <div className='pt-8 text-center'>
-                                    <a href="https://friendless-in-seattle.herokuapp.com/">
-                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg'>
-                                                        Demo
-                                                    </button>
-                                        </a>
-
-                                        <a href="https://github.com/Clos01/Friendless-in-seattle">
-                                                    <button className='text-center  rounded-lg px-4 py-3 bg-white text-gray-700 font-bold text-lg '>
-                                                    Code
-                                                    </button>
-                                        </a>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            </div>
-
-  
-          
-
-
-
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,15 +10,18 @@ const Home = () => {
             <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'> Carlos Rivas</h1>
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a full stack Developer </h2>
             <p className='text-[#8892b0] py-4  max-w-[700px] text-2xl'>Currently, I'm focused on building a responsive full-stack application.</p>
-            <dir>
+            <div>
+            <Link to='work' spy={true} smooth={true} duration={500}>
+            
                 <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 '>
                 View Work
                 <span className='group-hover:rotate-90 duration-300'>
      <HiArrowNarrowRight className='ml-3 ' /> 
                 </span>
                
-                </button>
-            </dir>
+                </button>  
+                 </Link>
+            </div>
         </div>
     </div>
   )
